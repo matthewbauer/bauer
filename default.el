e9c210b653f9710a5a9c62ccd3fe7c1564a2d29d
Bauer <baumatth@186590d272e7.ant.amazon.com>
2017-06-02 18:37:03 -0700

    Add in default settings.

--- default.el
+++ default.el
@@ -8,13 +8,300 @@
   (require 'use-package))
 (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
-(setq use-package-ensure-function 'ignore)
 
 (setenv "NIX_REMOTE" "daemon")
 (setenv "NIX_SSL_CERT_FILE" "/etc/ssl/certs/ca-bundle.crt")
 (setenv "EDITOR" "emacsclient -nw")
 (setenv "PATH" "~/.nix-profile/bin")
 (setenv "MANPATH" "~/.nix-profile/share/man")
+
+(custom-set-variables
+ '(ad-redefinition-action (quote accept))
+ '(apropos-do-all t)
+ '(auth-source-save-behavior t)
+ '(auto-revert-check-vc-info t)
+ '(auto-revert-use-notify t)
+ '(auto-revert-verbose nil)
+ '(backward-delete-char-untabify-method (quote hungry))
+ '(byte-compile-verbose nil)
+ '(c-eldoc-includes "" t)
+ '(company-auto-complete nil)
+ '(company-continue-commands
+   (quote
+    (not save-buffer save-some-buffers save-buffers-kill-terminal save-buffers-kill-emacs comint-previous-matching-input-from-input comint-next-matching-input-from-input)))
+ '(company-idle-delay 0.5)
+ '(company-minimum-prefix-length 2)
+ '(company-occurrence-weight-function (quote company-occurrence-prefer-any-closest))
+ '(company-show-numbers t)
+ '(company-tooltip-align-annotations t)
+ '(company-tooltip-flip-when-above t)
+ '(company-tooltip-limit 10)
+ '(compilation-always-kill t)
+ '(compilation-ask-about-save nil)
+ '(compilation-auto-jump-to-first-error t)
+ '(compilation-scroll-output (quote first-error))
+ '(completions-format (quote vertical))
+ '(counsel-find-file-at-point t)
+ '(counsel-mode-override-describe-bindings t)
+ '(cperl-clobber-lisp-bindings t)
+ '(cperl-continued-statement-offset 8)
+ '(cperl-electric-keywords nil)
+ '(cperl-electric-lbrace-space t)
+ '(cperl-electric-linefeed nil)
+ '(cperl-electric-parens nil)
+ '(cperl-font-lock t)
+ '(cperl-indent-level 4)
+ '(cperl-info-on-command-no-prompt t)
+ '(cperl-invalid-face nil)
+ '(cperl-lazy-help-time 3)
+ '(create-lockfiles nil)
+ '(css-indent-offset 2)
+ '(dired-dwim-target t)
+ '(dired-recursive-copies (quote always))
+ '(dired-recursive-deletes (quote top))
+ '(display-buffer-alist
+   (\`
+    (((\,
+       (rx bos
+           (or "*Flycheck errors*" "*Backtrace" "*Warnings" "*compilation" "*Help" "*less-css-compilation" "*Packages" "*magit-process" "*SQL" "*tldr")))
+      (display-buffer-reuse-window display-buffer-in-side-window)
+      (side . bottom)
+      (reusable-frames . visible)
+      (window-height . 0.33))
+     ("." nil
+      (reusable-frames . visible)))))
+ '(dumb-jump-quiet t)
+ '(dumb-jump-selector (quote ivy))
+ '(enable-recursive-minibuffers t)
+ '(erc-autoaway-idle-seconds 600)
+ '(erc-autoaway-use-emacs-idle t)
+ '(erc-autojoin-timing (quote ident))
+ '(erc-fill-prefix "          ")
+ '(erc-insert-timestamp-function (quote erc-insert-timestamp-left))
+ '(erc-interpret-mirc-color t)
+ '(erc-kill-buffer-on-part t)
+ '(erc-kill-queries-on-quit t)
+ '(erc-kill-server-buffer-on-quit t)
+ '(erc-prompt (lambda nil (concat "[" (buffer-name) "]")))
+ '(erc-prompt-for-nickserv-password nil)
+ '(erc-prompt-for-password nil)
+ '(erc-query-display (quote buffer))
+ '(erc-server-coding-system (quote (utf-8 . utf-8)))
+ '(erc-timestamp-format "%H:%M ")
+ '(erc-timestamp-only-if-changed-flag nil)
+ '(erc-track-exclude-types
+   (quote
+    ("JOIN" "NICK" "PART" "QUIT" "MODE" "324" "329" "332" "333" "353" "477")))
+ '(erc-try-new-nick-p nil)
+ '(eshell-banner-message "")
+ '(eshell-buffer-maximum-lines 20000)
+ '(eshell-buffer-shorthand t)
+ '(eshell-cd-on-directory t)
+ '(eshell-cmpl-autolist t)
+ '(eshell-cmpl-cycle-completions nil)
+ '(eshell-cmpl-cycle-cutoff-length 2)
+ '(eshell-cmpl-expand-before-complete t)
+ '(eshell-cmpl-ignore-case t)
+ '(eshell-complete-export-definition t)
+ '(eshell-cp-interactive-query t)
+ '(eshell-cp-overwrite-files nil)
+ '(eshell-default-target-is-dot t)
+ '(eshell-destroy-buffer-when-process-dies t)
+ '(eshell-highlight-prompt nil)
+ '(eshell-hist-ignoredups t)
+ '(eshell-history-size 10000)
+ '(eshell-list-files-after-cd t)
+ '(eshell-ln-interactive-query t)
+ '(eshell-modules-list
+   (quote
+    (eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob
+                  eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-term
+                  eshell-tramp eshell-unix eshell-xtra)))
+ '(eshell-mv-interactive-query t)
+ '(eshell-output-filter-functions
+   (quote
+    (eshell-handle-ansi-color eshell-handle-control-codes eshell-watch-for-password-prompt eshell-truncate-buffer)))
+ '(eshell-plain-echo-behavior nil)
+ '(eshell-prompt-function (quote epe-theme-lambda))
+ '(eshell-review-quick-commands t)
+ '(eshell-rm-interactive-query t)
+ '(eshell-send-direct-to-subprocesses nil)
+ '(eshell-visual-commands
+   (quote
+    ("vi" "screen" "top" "less" "more" "lynx" "ncftp" "pine" "tin" "trn" "elm" "nano")))
+ '(eval-expression-debug-on-error t)
+ '(explicit-bash-args (quote ("-c" "export EMACS=; stty echo; bash")))
+ '(explicit-shell-file-name "bash")
+ '(fased-completing-read-function (quote nil))
+ '(fill-column 80)
+ '(flycheck-check-syntax-automatically (quote (save idle-change mode-enabled)))
+ '(flycheck-global-modes
+   (quote
+    (not erc-mode message-mode git-commit-mode view-mode outline-mode text-mode)))
+ '(flycheck-idle-change-delay 0.8)
+ '(flycheck-standard-error-navigation t)
+ '(flyspell-abbrev-p nil)
+ '(flyspell-auto-correct nil)
+ '(flyspell-highlight-properties nil)
+ '(flyspell-incorrect-hook nil)
+ '(flyspell-issue-welcome-flag nil)
+ '(flyspell-use-meta-tab nil)
+ '(frame-title-format
+   (quote
+    ((:eval
+      (if
+          (buffer-file-name)
+          (abbreviate-file-name
+           (buffer-file-name))
+        "%b")))) t)
+ '(gc-cons-threshold 100000000)
+ '(history-delete-duplicates t)
+ '(history-length 20000)
+ '(iedit-toggle-key-default nil)
+ '(imenu-auto-rescan t)
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(initial-major-mode (quote fundamental-mode))
+ '(initial-scratch-message "")
+ '(ispell-extra-args (quote ("--sug-mode=ultra")))
+ '(ispell-program-name "aspell")
+ '(ispell-quietly t)
+ '(ivy-count-format "\"\"")
+ '(ivy-display-style nil)
+ '(ivy-minibuffer-faces nil)
+ '(ivy-use-virtual-buffers t)
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p nil)
+ '(js2-mode-show-parse-errors nil)
+ '(js2-mode-show-strict-warnings nil)
+ '(js2-strict-missing-semi-warning nil)
+ '(kill-do-not-save-duplicates t)
+ '(kill-whole-line t)
+ '(load-prefer-newer t)
+ '(mac-command-key-is-meta t)
+ '(mac-command-modifier 'meta)
+ '(mac-option-key-is-meta nil)
+ '(mac-frame-tabbing t)
+ '(mac-system-move-file-to-trash-use-finder t)
+ '(magit-clone-set-remote\.pushDefault t)
+ '(magit-completing-read-function (quote magit-builtin-completing-read))
+ '(magit-delete-by-moving-to-trash t)
+ '(magit-diff-options nil)
+ '(magit-ediff-dwim-show-on-hunks t)
+ '(magit-fetch-arguments nil)
+ '(magit-highlight-trailing-whitespace nil)
+ '(magit-highlight-whitespace nil)
+ '(magit-mode-hook
+   (quote
+    (magit-load-config-extensions magit-xref-setup bug-reference-mode)))
+ '(magit-no-confirm t)
+ '(magit-process-connection-type nil)
+ '(magit-process-find-password-functions (quote (magit-process-password-auth-source)))
+ '(magit-process-popup-time 15)
+ '(magit-push-always-verify nil)
+ '(magit-revision-mode-hook (quote (bug-reference-mode)))
+ '(magit-revision-show-gravatars (quote ("^Author:     " . "^Commit:
+  ")))
+ '(magit-save-repository-buffers (quote dontask))
+ '(magit-stage-all-confirm nil)
+ '(magit-unstage-all-confirm nil)
+ '(make-backup-files nil)
+ '(network-security-level (quote medium))
+ '(next-error-recenter (quote (4)))
+ '(nrepl-log-messages t)
+ '(nsm-save-host-names t)
+ '(parens-require-spaces t)
+ '(parse-sexp-ignore-comments t)
+ '(projectile-completion-system (quote ivy))
+ '(projectile-enable-caching t)
+ '(projectile-enable-idle-timer t)
+ '(projectile-idle-timer-hook nil)
+ '(projectile-ignored-project-function (quote file-remote-p))
+ '(projectile-mode-line
+   (quote
+    (:eval
+     (if
+         (projectile-project-p)
+         (format " P[%s]"
+                 (projectile-project-name))
+       ""))))
+ '(projectile-switch-project-action (quote projectile-dired))
+ '(projectile-verbose nil)
+ '(reb-re-syntax (quote string))
+ '(require-final-newline t)
+ '(restart-emacs-restore-frames nil)
+ '(ring-bell-function (quote ignore))
+ '(rtags-completions-enabled t)
+ '(rtags-display-result-backend (quote ivy))
+ '(rtags-imenu-syntax-highlighting 10)
+ '(ruby-insert-encoding-magic-comment nil)
+ '(safe-local-variable-values
+   (quote
+    ((eval c-set-offset
+           (quote access-label)
+           (quote -))
+     (eval c-set-offset
+           (quote substatement-open)
+           0)
+     (eval c-set-offset
+           (quote arglist-cont-nonempty)
+           (quote +))
+     (eval c-set-offset
+           (quote arglist-cont)
+           0)
+     (eval c-set-offset
+           (quote arglist-intro)
+           (quote +))
+     (eval c-set-offset
+           (quote inline-open)
+           0)
+     (eval c-set-offset
+           (quote defun-open)
+           0)
+     (eval c-set-offset
+           (quote innamespace)
+           0)
+     (indicate-empty-lines . t))))
+ '(same-window-buffer-names
+   (quote
+    ("*eshell*" "*shell*" "*mail*" "*inferior-lisp*" "*ielm*" "*scheme*")))
+ '(save-abbrevs (quote silently))
+ '(savehist-additional-variables (quote (search-ring regexp-search-ring)))
+ '(savehist-autosave-interval 60)
+ '(send-mail-function (quote smtpmail-send-it))
+ '(sentence-end-double-space nil)
+ '(sh-learn-basic-offset t)
+ '(shell-completion-execonly nil)
+ '(shell-input-autoexpand nil)
+ '(sp-autoskip-closing-pair (quote always))
+ '(sp-base-key-bindings (quote paredit))
+ '(sp-hybrid-kill-entire-symbol nil)
+ '(term-input-autoexpand t)
+ '(term-input-ring-file-name t)
+ '(tls-checktrust t)
+ '(undo-limit 800000)
+ '(uniquify-after-kill-buffer-p t)
+ '(uniquify-ignore-buffers-re "^\\*")
+ '(uniquify-separator "/")
+ '(use-package-always-defer t)
+ '(use-package-always-ensure nil)
+ '(use-package-ensure-function 'ignore)
+ '(use-package-enable-imenu-support t)
+ '(use-package-expand-minimally t)
+ '(use-package-verbose nil)
+ '(vc-allow-async-revert t)
+ '(vc-command-messages t)
+ '(vc-git-diff-switches (quote ("-w" "-U3")))
+ '(visible-bell nil)
+ '(visible-cursor nil)
+ '(whitespace-line-column 80)
+ '(yas-prompt-functions
+   (quote
+    (yas-ido-prompt yas-completing-prompt yas-no-prompt)))
+ '(yas-triggers-in-field t)
+ '(yas-wrap-around-region t))
 
 ;;
 ;; builtins
@@ -788,8 +1075,7 @@ POINT ?"
 
   (with-eval-after-load "esh-opt"
     (autoload 'epe-theme-lambda "eshell-prompt-extras")
-    (setq eshell-highlight-prompt nil
-	  eshell-prompt-function 'epe-theme-lambda))
+    (setq eshell-prompt-function 'epe-theme-lambda))
 
   (setenv "PAGER" "cat")
   (setenv "EDITOR" "emacsclient -nq")
@@ -797,11 +1083,6 @@ POINT ?"
   (add-hook 'eshell-mode-hook
             (lambda ()
               (define-key eshell-mode-map [(control ?u)] nil))))
-
-(use-package eshell-fringe-status
-  :commands eshell-fringe-status-mode
-  :init
-  (add-hook 'eshell-mode-hook 'eshell-fringe-status-mode))
 
 (use-package expand-region
   :commands er/expand-region
@@ -1050,8 +1331,13 @@ POINT ?"
    ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
 
 (use-package mwim
-  :bind (("C-a" . mwim-beginning-of-code-or-line)
-         ("C-e" . mwim-end-of-code-or-line)))
+  :commands (mwim-beginning-of-line mwim-end)
+  ;; :bind (("C-a" . mwim-beginning-of-code-or-line)
+  ;;        ("C-e" . mwim-end-of-code-or-line))
+  :init
+  (global-set-key [remap move-beginning-of-line] #'mwim-beginning)
+  (global-set-key [remap move-end-of-line] #'mwim-end)
+  )
 
 (use-package neotree
   :commands (neotree-show neotree-hide)
--- init.el
+++
@@ -1,7 +0,0 @@
-
-;; (package-initialize)
-
-(setq custom-file (expand-file-name "settings.el" user-emacs-directory))
-(load custom-file)
-
-;; (require 'default)
