d783435e36a801586df2db6e316c5d2db4acd47f
Matthew Bauer <mjbauer95@gmail.com>
2017-06-12 22:34:00 -0700

    Get rid of plist stuff
    wasn’t working properly

--- default.el
+++ default.el
@@ -496,20 +496,6 @@ you can use this command to copy text from a read-only buffer.
    nil 'fullscreen
    (when (not (frame-parameter nil 'fullscreen)) 'maximized)))
 
-;; Allow editing of binary .plist files.
-(add-to-list 'jka-compr-compression-info-list
-             ["\\.plist$"
-              "converting text XML to binary plist"
-              "plutil"
-              ("-convert" "binary1" "-o" "-" "-")
-              "converting binary plist to text XML"
-              "plutil"
-              ("-convert" "xml1" "-o" "-" "-")
-              nil nil "bplist"])
-
-;;It is necessary to perform an update!
-(jka-compr-update)
-
 (defvar lisp-modes '(emacs-lisp-mode
                      inferior-emacs-lisp-mode
                      ielm-mode
