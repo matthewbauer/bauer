385d99d7aebd6cae22274d73806e6ed5b69dd015
Matthew Bauer <mjbauer95@gmail.com>
2017-06-12 23:01:35 -0700

    switch to meghanada

--- default.el
+++ default.el
@@ -1025,9 +1025,41 @@ FUNC is run when MODES are loaded."
               (setq eldoc-documentation-function
                     'esh-help-eldoc-command))))
 
-(use-package jdee
+;; (use-package jdee
+;;   :init
+;;   (setq jdee-server-dir "@jdeeserver@"))
+
+(use-package realgud)
+
+(use-package autodisass-java-bytecode)
+
+(use-package meghanada
   :init
-  (setq jdee-server-dir "@jdeeserver@"))
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (google-set-c-style)
+              (google-make-newline-indent)
+              (meghanada-mode t)
+              (smartparens-mode t)
+              (rainbow-delimiters-mode t)
+              (highlight-symbol-mode t)
+              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+
+  :config
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq meghanada-server-remote-debug t)
+  (setq meghanada-javac-xlint "-Xlint:all,-processing")
+
+  :bind
+  (:map meghanada-mode-map
+        ("C-S-t" . meghanada-switch-testcase)
+        ("M-RET" . meghanada-local-variable)
+        ("C-M-." . helm-imenu)
+        ("C-z" . hydra-meghanada/body))
+
+  :commands (meghanada-mode))
 
 (use-package eshell
   :bind (("C-x e" . eshell-new))
