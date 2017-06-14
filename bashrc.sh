01ca1726ea16e113e83c069d0cf18a2682579641
Matthew Bauer <baumatth@amazon.com>
2017-06-13 21:21:02 -0700

    Disable completion
    is broken for some reason on macbook

--- bashrc.sh
+++ bashrc.sh
@@ -20,20 +20,20 @@ shopt -s histappend histreedit histverify
 
 export SHELL_SESSION_HISTORY=0
 
-if [ -z "$INSIDE_EMACS" ] || [ "$EMACS_BASH_COMPLETE" = "t" ]; then
-    # Check whether we're running a version of Bash that has support for
-    # programmable completion. If we do, enable all modules installed in
-    # the system (and user profile).
-    if shopt -q progcomp &>/dev/null; then
-	nullglobStatus=$(shopt -p nullglob)
-	shopt -s nullglob
-	for m in "@out@/etc/bash_completion.d/"* "@out@/share/bash-completion/completions/"*; do
-            . $m
-	done
-	eval "$nullglobStatus"
-	unset nullglobStatus p m
-    fi
-fi
+# if [ -z "$INSIDE_EMACS" ] || [ "$EMACS_BASH_COMPLETE" = "t" ]; then
+#     # Check whether we're running a version of Bash that has support for
+#     # programmable completion. If we do, enable all modules installed in
+#     # the system (and user profile).
+#     if shopt -q progcomp &>/dev/null; then
+#       nullglobStatus=$(shopt -p nullglob)
+#       shopt -s nullglob
+#       for m in "@out@/etc/bash_completion.d/"* "@out@/share/bash-completion/completions/"*; do
+#             . $m
+#       done
+#       eval "$nullglobStatus"
+#       unset nullglobStatus p m
+#     fi
+# fi
 
 # prompt
 BLUE="\[$(/usr/bin/tput setaf 4)\]"
