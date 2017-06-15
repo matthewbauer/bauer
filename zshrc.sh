afc56afb95ef302f288c5c61d8063f2d89a82d78
Matthew Bauer <baumatth@amazon.com>
2017-06-14 18:41:44 -0700

    rework zsh prompt

--- zshrc.sh
+++ zshrc.sh
@@ -127,7 +127,8 @@ function parse_git_dirty() {
 ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
 
 local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT='${ret_status} %{$fg[blue]%}%m:%{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+#PROMPT='${ret_status} %{$fg[blue]%}%m:%{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info) $'
+PROMPT='%{$fg[blue]%}%m:%{$fg[cyan]%}%c%{$reset_color%} $'
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
