76f3bd35600b3b8e40c922c300a31a9f4d8fafe1
Matthew Bauer <mjbauer95@gmail.com>
2017-07-06 13:58:08 -0700

    remove old cc-mode settings

--- default.el
+++ default.el
@@ -554,8 +554,7 @@ typical word processor."
 (use-package abbrev
   :diminish abbrev-mode
   :demand
-  :config
-  (setq-default abbrev-mode t))
+  :config (setq-default abbrev-mode t))
 
 (use-package ace-window
   :bind ("M-o" . ace-window))
@@ -623,171 +622,11 @@ typical word processor."
          ("\\.cpp\\'"                 . c++-mode)
          ("\\.c++\\'"                 . c++-mode)
          ("\\.mm\\'"                  . c++-mode))
-  :preface
-  (defun my-paste-as-check ()
-    (interactive)
-    (save-excursion
-      (insert "/*\n")
-      (let ((beg (point)) end)
-        (yank)
-        (setq end (point-marker))
-        (goto-char beg)
-        (while (< (point) end)
-          (forward-char 2)
-          (insert "CHECK: ")
-          (forward-line 1)))
-      (insert "*/\n")))
-
-  (defvar printf-index 0)
-
-  (defun insert-counting-printf (arg)
-    (interactive "P")
-    (if arg
-        (setq printf-index 0))
-    (if t
-        (insert (format "std::cerr << \"step %d..\" << std::endl;\n"
-                        (setq printf-index (1+ printf-index))))
-      (insert (format "printf(\"step %d..\\n\");\n"
-                      (setq printf-index (1+ printf-index)))))
-    (forward-line -1)
-    (indent-according-to-mode)
-    (forward-line))
-
-  (defun my-c-mode-common-hook ()
-    ;; (ggtags-mode 1)
-    (hs-minor-mode 1)
-    (hide-ifdef-mode 1)
-    ;; (whitespace-mode 1)
-    (which-function-mode 1)
-    (company-mode 1)
-    (bug-reference-prog-mode 1)
-
-    (bind-key "<return>" #'newline-and-indent c-mode-base-map)
-
-    (unbind-key "M-j" c-mode-base-map)
-    (bind-key "C-c C-i" #'c-includes-current-file c-mode-base-map)
-    (bind-key "C-c C-y" #'my-paste-as-check c-mode-base-map)
-
-    (set (make-local-variable 'parens-require-spaces) nil)
-    (setq indicate-empty-lines t)
-    (setq fill-column 72)
-
-    (bind-key "M-q" #'c-fill-paragraph c-mode-base-map)
-
-    (let ((bufname (buffer-file-name)))
-      (when bufname
-        (cond
-         ((string-match "/ledger/" bufname)
-          (c-set-style "ledger"))
-         ((string-match "/edg/" bufname)
-          (c-set-style "edg"))
-         (t
-          (c-set-style "clang")))))
-
-    (font-lock-add-keywords 'c++-mode '(("\\<\\(assert\\|DEBUG\\)("
-                                         1 font-lock-warning-face t))))
-
   :config
-
   (use-package c-eldoc
     :commands c-turn-on-eldoc-mode
     :init (add-hook 'c-mode-common-hook 'c-turn-on-eldoc-mode))
-
-  (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-
-  (bind-key "#" #'self-insert-command c-mode-base-map)
-  (bind-key "{" #'self-insert-command c-mode-base-map)
-  (bind-key "}" #'self-insert-command c-mode-base-map)
-  (bind-key "/" #'self-insert-command c-mode-base-map)
-  (bind-key "*" #'self-insert-command c-mode-base-map)
-  (bind-key ";" #'self-insert-command c-mode-base-map)
-  (bind-key "," #'self-insert-command c-mode-base-map)
-  (bind-key ":" #'self-insert-command c-mode-base-map)
-  (bind-key "(" #'self-insert-command c-mode-base-map)
-  (bind-key ")" #'self-insert-command c-mode-base-map)
-  (bind-key "<" #'self-insert-command c++-mode-map)
-  (bind-key ">" #'self-insert-command c++-mode-map)
-
-  (add-to-list 'c-style-alist
-               '("edg"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . +)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (add-to-list 'c-style-alist
-               '("ledger"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . 0)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (add-to-list 'c-style-alist
-               '("clang"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . 0)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . ""))))
+  )
 
 (use-package coffee-mode
   :mode (("\\.coffee\\'" . coffee-mode)))
