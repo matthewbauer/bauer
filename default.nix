489340b4d786386116ceeea09f2675e6d3925864
Matthew Bauer <mjbauer95@gmail.com>
2017-09-13 10:03:02 -0500

    Put site-paths in its own file again
    Can’t figure out why babel doesn’t like having it in README.org

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ let # nixpkgs-17.09-darwin at 2017-09-10
     nixpkgsRev = "b50193f0ca3e9a0d8db2bedfae628995653197d7";
     defaultNixpkgs = builtins.fetchTarball
       "github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+
 in { nixpkgs ? defaultNixpkgs, nixpkgs_ ? import nixpkgs {} }:
 with nixpkgs_;
 
@@ -14,6 +15,7 @@ mkdir -p $out
 cd $out
 cp ${./README.org} README.org
 cp -r ${./lisp} $out/lisp
+cp ${./site-paths.el.in} $out/site-paths.el.in
 emacs --batch -l ob-tangle --eval "(org-babel-tangle-file \"README.org\")"
 cp bauer.nix default.nix
          '';
