0c8265ab55e80f797534f055f1cf33bdf037863e
Matthew Bauer <mjbauer95@gmail.com>
2017-09-13 09:33:16 -0500

    Move everything to README.org
    Needs to be cleaned up some but seems to be working as intended.

--- default.nix
+++ default.nix
@@ -1,19 +1,20 @@
-let
-  # channel = "nixpkgs-17.09-darwin";
-  # defaultNixpkgs = builtins.fetchTarball
-  #   "github.com/NixOS/nixpkgs-channels/archive/${channel}.tar.gz";
+let # nixpkgs-17.09-darwin at 2017-09-10
+    nixpkgsRev = "b50193f0ca3e9a0d8db2bedfae628995653197d7";
+    defaultNixpkgs = builtins.fetchTarball
+      "github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+in { nixpkgs ? defaultNixpkgs, nixpkgs_ ? import nixpkgs {} }:
+with nixpkgs_;
 
-  # nixpkgs-17.09-darwin at 2017-09-10
-  nixpkgsRev = "b50193f0ca3e9a0d8db2bedfae628995653197d7";
-  defaultNixpkgs = builtins.fetchTarball
-    "github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-in
-
-{ nixpkgs ? defaultNixpkgs }:
-
-with import nixpkgs {};
-
-let config = import ./config.nix;
-    pkgs_ = config.packageOverrides pkgs;
-in
-  pkgs_.bauer
+import (stdenv.mkDerivation {
+         name = "README";
+         unpackPhase = "true";
+         buildInputs = [ emacs ];
+         installPhase = ''
+mkdir -p $out
+cd $out
+cp ${./README.org} README.org
+cp -r ${./lisp} $out/lisp
+emacs --batch -l ob-tangle --eval "(org-babel-tangle-file \"README.org\")"
+cp bauer.nix default.nix
+         '';
+}) {}
--- etc-profile.sh
+++
@@ -1,9 +0,0 @@
-export PREFIX=@out@
-
-if [ -d @out@/etc/profile.d ]; then
-  for i in @out@/etc/profile.d/*.sh; do
-    if [ -r $i ]; then
-      . $i
-    fi
-  done
-fi
