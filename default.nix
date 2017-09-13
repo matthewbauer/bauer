34539debf49dd28960658336635a8d05da9ed5d3
Matthew Bauer <mjbauer95@gmail.com>
2017-09-13 12:45:05 -0500

    Simplify code

--- default.nix
+++ default.nix
@@ -2,21 +2,19 @@ let # nixpkgs-17.09-darwin at 2017-09-10
     nixpkgsRev = "b50193f0ca3e9a0d8db2bedfae628995653197d7";
     defaultNixpkgs = builtins.fetchTarball
       "github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-
-in { nixpkgs ? defaultNixpkgs, nixpkgs_ ? import nixpkgs {} }:
-with nixpkgs_;
+in { nixpkgs ? defaultNixpkgs, nixpkgs' ? import nixpkgs {} }: with nixpkgs';
 
 import (stdenv.mkDerivation {
-         name = "README";
-         unpackPhase = "true";
-         buildInputs = [ emacs ];
-         installPhase = ''
+          name = "README";
+          unpackPhase = "true";
+          buildInputs = [ emacs ];
+          installPhase = ''
 mkdir -p $out
 cd $out
 cp ${./README.org} README.org
-cp -r ${./lisp} $out/lisp
 cp ${./site-paths.el.in} $out/site-paths.el.in
+cp -r ${./lisp} $out/lisp
 emacs --batch -l ob-tangle --eval "(org-babel-tangle-file \"README.org\")"
 cp bauer.nix default.nix
-         '';
+          '';
 }) {}
