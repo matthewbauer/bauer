39eced796da02ee9f7d58b47d927310a2664819e
Matthew Bauer <mjbauer95@gmail.com>
2018-02-27 14:54:40 -0600

    Merge code blocks

--- default.nix
+++ default.nix
@@ -4,16 +4,12 @@
 }: with nixpkgs';
 
 import (runCommand "README" { buildInputs = [ emacs ]; } ''
-
-mkdir -p $out
-cd $out
-cp -r ${./site-lisp} $out/site-lisp
-cp ${./README.org} README.org
-emacs --batch --quick \
-      -l ob-tangle \
-      --eval "(org-babel-tangle-file \"README.org\")"
-cp bauer.nix default.nix
-
-''
-
-) {inherit nixpkgs';}
+  mkdir -p $out
+  cd $out
+  cp -r ${./site-lisp} site-lisp
+  cp ${./README.org} README.org
+  emacs --batch --quick \
+        -l ob-tangle \
+        --eval "(org-babel-tangle-file \"README.org\")"
+  cp bauer.nix default.nix
+'') {inherit nixpkgs';}
