d4772bf727d7a58e3c0312d41d5481ffdd890db9
Matthew Bauer <mjbauer95@gmail.com>
2018-05-02 20:57:08 -0500

    Ensure site-lisp exists before copying.
    Will now download relevant lisp from
    https://matthewbauer.us/bauer/site-lisp/.

--- default.nix
+++ default.nix
@@ -1,15 +1,18 @@
-{ nixpkgs-url ? "nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz"
-, nixpkgs ? builtins.fetchTarball nixpkgs-url
-, nixpkgs' ? import nixpkgs {}
-}: with nixpkgs';
+{ channel ? (if builtins.currentSystem == "x86_64-darwin"
+             then "nixpkgs-unstable"
+             else "nixos-unstable")
+, nixpkgs-url ? "nixos.org/channels/${channel}/nixexprs.tar.xz"
+, nixpkgs ? import (builtins.fetchTarball nixpkgs-url) {}
+}: with nixpkgs;
 
-import (runCommand "README" { buildInputs = [ emacs git ]; } ''
-  mkdir -p $out
+import (runCommand "README" { buildInputs = [ emacs git ]; } (''
+  install -D ${./README.org} $out/README.org
   cd $out
+'' + lib.optionalString (builtins.pathExists ./site-lisp) ''
   cp -r ${./site-lisp} site-lisp
-  cp ${./README.org} README.org
+'' + ''
   emacs --batch --quick \
 	-l ob-tangle \
 	--eval "(org-babel-tangle-file \"README.org\")"
   cp bauer.nix default.nix
-'') {inherit nixpkgs';}
+'')) { inherit nixpkgs; }
