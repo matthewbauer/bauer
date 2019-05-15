8b022b29cd73398185b89c47941115902a540771
Matthew Bauer <mjbauer95@gmail.com>
2019-05-15 15:19:24 -0400

    Regen

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+update() {
+  ./update.sh
+  git add .
+  git commit -m "Regen"
+}
+
+setup() {
+  git stash push
+  # update
+  git checkout gh-pages
+}
+
+cleanup() {
+  git checkout master
+  git stash pop
+}
+
+setup
+trap cleanup EXIT
+
+git fetch origin
+git reset --hard origin/gh-pages
+
+git merge --no-edit master
+update
+git push origin gh-pages
