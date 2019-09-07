469744b638ae375f6de0194eac63097590405733
Matthew Bauer <mjbauer95@gmail.com>
2019-09-06 23:50:57 -0400

    Load nix-mode in html gen

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ emacs --batch \
       --eval "(org-babel-tangle-file \"README.org\")"
 emacs README.org --batch \
   --eval "(setq org-html-htmlize-output-type 'css)" \
+  -l nix-mode \
   -f org-html-export-to-html
