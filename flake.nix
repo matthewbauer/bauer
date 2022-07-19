b06bae5921eebd7da71dd3603c00f6f677a59557
Matthew Bauer <mjbauer95@gmail.com>
2022-07-18 17:01:38 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -2,10 +2,13 @@
   description = "an Emacs+Nix IDE";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.emacs-overlay.url = "github:nix-community/emacs-overlay";
+  inputs.emacs-overlay = {
+    url = "github:nix-community/emacs-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = { self, nixpkgs, emacs-overlay }: let
-    systems = [ "x86_64-linux" ];
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
   in {
@@ -24,33 +27,32 @@
               --eval "(org-babel-tangle-file \"README.org\")" > /dev/null
         cp bauer.nix default.nix
       '')) { inherit pkgs emacs-overlay evalPkgs system; };
+      default = self.packages.${system}.bauer;
     });
-
-    defaultPackage = forAllSystems (system: self.packages.${system}.bauer);
 
     apps = forAllSystems (system: {
       bauer = {
         type = "app";
         program = "${self.packages.${system}.bauer}/bin/run";
       };
+      default = self.apps.${system}.bauer;
     });
 
-    defaultApp = forAllSystems (system: self.apps.${system}.bauer);
-
-    devShell = forAllSystems (system:
-      with nixpkgsFor.${system};
-      stdenv.mkDerivation {
+    devShells = forAllSystems (system: {
+      bauer = with nixpkgsFor.${system}; stdenv.mkDerivation {
         name = "bauer";
         nativeBuildInputs = [ pkgs.emacs git ];
         shellHook = ''
           echo Run ./update.sh to generate files.
         '';
-      });
+      };
+      default = self.devShells.${system}.bauer;
+    });
 
     nixosModules.bauer = {
       imports = [ ./module.nix ];
     };
   };
 
-  # nixConfig.allow-import-from-derivation = true;
+  nixConfig.allow-import-from-derivation = true;
 }
